905eaf94268e79e97c1268faf12359f57c2f8f39
hansmbua <hansmbua@yahoo.com>
2024-06-27 23:09:24 +0200

    added disable on test

--- SimpleCalculator/src/Start.java
+++ SimpleCalculator/src/Start.java
@@ -1,5 +1,6 @@
 
-public class  Start {
+public class Start {
+
     public static void main(String[] args) {
         Person person = new Person();
         person.giveName("Hans");
@@ -8,5 +9,24 @@ public class  Start {
         person.giveLastName("Ebai");
         System.out.println(person.getLastName());
 
+        MaxNumber();
     }
+
+    public static void MaxNumber() {
+        int[] array = {1, 13, 4, 5};
+
+        int max = 5;
+        if (1 > max) {
+            max = 1;
+        } else if (13 > max) {
+            max = 13;
+        } else if (4 > max) {
+            max = 4;
+        } else if (5 > max) {
+            max = 5;
+        }
+        System.out.println(max);
+    }
+
+
 }
