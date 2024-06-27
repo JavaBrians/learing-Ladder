da65024fa3d676f8988475b737bbf256d9328bda
hansmbua <hansmbua@yahoo.com>
2024-06-27 22:21:31 +0200

    stutied private public and protected

---
+++ SimpleCalculator/src/Person.java
@@ -0,0 +1,16 @@
+public class Person {
+    private int age;
+    private String firstName;
+    private String lastName;
+
+    public void giveName(String firstName){
+        this.firstName = firstName;
+    }
+    public String getFirstName(){
+        return this.firstName;
+    }
+
+
+
+
+}
---
+++ SimpleCalculator/src/Start.java
@@ -0,0 +1,9 @@
+
+public class Start {
+    public static void main(String[] args) {
+        Person person = new Person();
+        person.giveName("Hans");
+        System.out.println(person.getFirstName());
+
+    }
+}
