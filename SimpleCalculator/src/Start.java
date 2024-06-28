7a97c6800de915a5b3013aac5b925fc865eccd28
hansmbua <hansmbua@yahoo.com>
2024-06-28 23:09:04 +0200

    intro math operators

--- SimpleCalculator/src/Start.java
+++ SimpleCalculator/src/Start.java
@@ -1,7 +1,9 @@
+import java.util.Scanner;
 
 public class Start {
 
     public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
 //        Person person = new Person();
 //        person.giveName("Hans");
 //        System.out.println(person.getFirstName());
@@ -12,13 +14,28 @@ public class Start {
 //        moreSyntaxErrorExample.run();
 //      int number =  MoreSyntaxErrorExample.number;
 
-        int number = 6;
-        FactorialDebugExample factorial = new FactorialDebugExample();
-        int fac = factorial.calculateFactorial(number);
-        System.out.println("The factorial of " + number + " is: " + fac);
+//        int number = 6;
+//        FactorialDebugExample factorial = new FactorialDebugExample();
+//        int fac = factorial.calculateFactorial(number);
+//        System.out.println("The factorial of " + number + " is: " + fac);
+        int sum = 0;
+        System.out.println("enter the first number you want to add");
+        int number1 = Integer.parseInt(keyboard.nextLine()); // 4
+        System.out.println("choose operator you want to use example -, +, /");
+        String userOperatorChoose = keyboard.nextLine();
+
+        switch (userOperatorChoose){
+            case "+":
+                System.out.println("enter the number you want to add");
+                int number2 = keyboard.nextInt();
+                sum = number1 + number2;
+                System.out.printf("%d + %d = %d",number1,number2,sum);
+        }
 
 
     }
+
+
 
 
 }
