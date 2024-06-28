a82081a9269248c1eea008081d4b033e94e95827
ebai alice <alice.ebai@student.howest.be>
2024-06-28 21:29:49 +0200

    fixed syntax

--- SimpleCalculator/src/SyntaxErrorExample.java
+++ SimpleCalculator/src/SyntaxErrorExample.java
@@ -1,25 +1,25 @@
 public class SyntaxErrorExample {
 
     public void run(){
-        System.out.println("Welcome to the Java Syntax Error Practice Program);
+        System.out.println("Welcome to the Java Syntax Error Practice Program");
 
-        int number = 10
+        int number = 10;
         System.out.println("The number is: " + number);
 
-        String greeting = Hello, World!;
-        System.out.println(greeting)
+        String greeting = ("HelloWorld");
+        System.out.println(greeting);
 
-        if number > 5 {
+        if(number > 5)  {
             System.out.println("Number is greater than 5");
         } else {
-            System.out.println("Number is 5 or less")
+            System.out.println("Number is 5 or less");
         }
 
         for(int i = 0; i < 5; i++) {
-            System.out.println("i is: " + i)
+            System.out.println("i is: " + i);
         }
 
-        System.out.println("End of program."
+        System.out.println("End of program.");
 
     }
 
