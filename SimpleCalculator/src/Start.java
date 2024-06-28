620dffa6ab06068652f3bb14c47f46cd70612c80
hansmbua <hansmbua@yahoo.com>
2024-06-28 21:22:17 +0200

    to fix

--- SimpleCalculator/src/Start.java
+++ SimpleCalculator/src/Start.java
@@ -2,31 +2,25 @@
 public class Start {
 
     public static void main(String[] args) {
-        Person person = new Person();
-        person.giveName("Hans");
-        System.out.println(person.getFirstName());
-
-        person.giveLastName("Ebai");
-        System.out.println(person.getLastName());
-
-        MaxNumber();
+//        Person person = new Person();
+//        person.giveName("Hans");
+//        System.out.println(person.getFirstName());
+//
+//        person.giveLastName("Ebai");
+//        System.out.println(person.getLastName());
+        SyntaxErrorExample syntaxErrorExample = new SyntaxErrorExample();
+        syntaxErrorExample.run();
     }
 
-    public static void MaxNumber() {
-        int[] array = {1, 13, 4, 5};
-
-        int max = 5;
-        if (1 > max) {
-            max = 1;
-        } else if (13 > max) {
-            max = 13;
-        } else if (4 > max) {
-            max = 4;
-        } else if (5 > max) {
-            max = 5;
+    public static void forTest(){
+        for(int i = 0; i < 4; i++){
+            System.out.println();
         }
-        System.out.println(max);
     }
+
+
+
+
 
 
 }
---
+++ SimpleCalculator/src/SyntaxErrorExample.java
@@ -0,0 +1,27 @@
+public class SyntaxErrorExample {
+
+    public void run(){
+        System.out.println("Welcome to the Java Syntax Error Practice Program);
+
+        int number = 10
+        System.out.println("The number is: " + number);
+
+        String greeting = Hello, World!;
+        System.out.println(greeting)
+
+        if number > 5 {
+            System.out.println("Number is greater than 5");
+        } else {
+            System.out.println("Number is 5 or less")
+        }
+
+        for(int i = 0; i < 5; i++) {
+            System.out.println("i is: " + i)
+        }
+
+        System.out.println("End of program."
+
+    }
+
+
+}
