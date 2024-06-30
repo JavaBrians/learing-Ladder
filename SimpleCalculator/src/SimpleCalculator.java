c3b138fc73d6f41c05bac9e1ca420cee3e854555
ebai alice <alice.ebai@student.howest.be>
2024-06-30 21:03:50 +0200

    simple calculator

---
+++ SimpleCalculator/src/SimpleCalculator.java
@@ -0,0 +1,36 @@
+public class SimpleCalculator {
+
+
+
+
+
+    public int addition(int a, int b){
+        return a + b;
+    }
+    public int subtraction(int a, int b){
+        return a + b;
+    }
+    public int multiplication(int a, int b){
+        return a * b;
+    }
+    public int division(int a, int b){
+        return a / b;
+    }
+  public String UserOperator(char operator){
+        ;
+
+        switch(operator){
+            case '+':
+                return "+";
+            case '-':
+                return "-";
+            case '*':
+                return "*";
+            case '/':
+                return "/";
+            default:
+                return "Invalid operator";
+        }
+
+    }
+}
--- SimpleCalculator/src/Start.java
+++ SimpleCalculator/src/Start.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
 
+import static java.lang.StringUTF16.charAt;
+
 public class Start {
 
     public static void main(String[] args) {
@@ -18,24 +20,44 @@ public class Start {
 //        FactorialDebugExample factorial = new FactorialDebugExample();
 //        int fac = factorial.calculateFactorial(number);
 //        System.out.println("The factorial of " + number + " is: " + fac);
-        int sum = 0;
-        System.out.println("enter the first number you want to add");
-        int number1 = Integer.parseInt(keyboard.nextLine()); // 4
-        System.out.println("choose operator you want to use example -, +, /");
-        String userOperatorChoose = keyboard.nextLine();
+        //
+        //      int sum = 0;
+        //       System.out.println("enter the first number you want to add");
+        //       int number1 = Integer.parseInt(keyboard.nextLine()); // 4
+        //       System.out.println("choose operator you want to use example -, +, /");
+        //      String userOperatorChoose = keyboard.nextLine();
 
-        switch (userOperatorChoose){
-            case "+":
-                System.out.println("enter the number you want to add");
-                int number2 = keyboard.nextInt();
-                sum = number1 + number2;
-                System.out.printf("%d + %d = %d",number1,number2,sum);
+        //       switch (userOperatorChoose){
+        //           case "+":
+        //              System.out.println("enter the number you want to add");
+//                int number2 = keyboard.nextInt();
+//                sum = number1 + number2;
+        //               System.out.printf("%d + %d = %d",number1,number2,sum);
+
+        //       }
+        SimpleCalculator calculator = new SimpleCalculator();
+        while (true) {
+            System.out.println("enter the first number");
+            String input = String.valueOf(Integer.parseInt(keyboard.next()));
+            System.out.println("choose operator you want to use example -, +, /");
+            String userOperatorChoose = keyboard.next();
+            if (calculator.UserOperator(calculator.UserOperator()!= userOperatorChoose) {
+                System.out.println("chose a valid operator");
+            }esle {
+
+            }
+            System.out.println("enter the second number");
+            String input2 = String.valueOf(Integer.parseInt(keyboard.next()));
+            }
+
+
+
+
+            }
+
+
         }
 
 
-    }
 
 
-
-
-}
