3cd8ecd8c1ccdc1178cc8db396625647a7decb4e
ebai alice <alice.ebai@student.howest.be>
2024-07-04 21:10:46 +0200

    solution of simple calculator

--- SimpleCalculator/src/SimpleCalculator.java
+++ SimpleCalculator/src/SimpleCalculator.java
@@ -4,31 +4,23 @@ public class SimpleCalculator {
 
 
 
-    public int addition(int a, int b){
-        return a + b;
-    }
-    public int subtraction(int a, int b){
-        return a + b;
-    }
-    public int multiplication(int a, int b){
-        return a * b;
-    }
-    public int division(int a, int b){
-        return a / b;
-    }
-  public String UserOperator(char operator){
-        switch(operator){
-            case '+':
-                return "+";
-            case '-':
-                return "-";
-            case '*':
-                return "*";
-            case '/':
-                return "/";
+
+
+    public  int UserOperator(int num1, String operator,int num2) {
+        switch (operator) {
+            case "+":
+                return num1 + num2;
+            case "-":
+                return num1 - num2;
+            case "*":
+                return num1 * num2;
+            case "/":
+                return num1 / num2;
             default:
-                return "Invalid operator";
+                System.out.println("Invalid operator.");
         }
+
+        return 0;
 
     }
 }
--- SimpleCalculator/src/Start.java
+++ SimpleCalculator/src/Start.java
@@ -5,6 +5,9 @@ public class Start {
 
     public static void main(String[] args) {
         Scanner keyboard = new Scanner(System.in);
+        SimpleCalculator calculator= new SimpleCalculator();
+
+
 //        Person person = new Person();
 //        person.giveName("Hans");
 //        System.out.println(person.getFirstName());
@@ -25,22 +28,23 @@ public class Start {
         // todo: improve this program do not build a new program
         //todo: understand the code and make a plan of action and work according to your plan
 
-        int sum;
+        //int sum;
         System.out.println("enter the first number you want to add");
         int number1 = Integer.parseInt(keyboard.nextLine()); // 4
         System.out.println("choose operator you want to use example -, +, /");
-        String userOperatorChoose = keyboard.nextLine();
-        System.out.println("enter the number you want to add");
-        int number2 = keyboard.nextInt();
+        String operator = keyboard.nextLine();
 
-        switch (userOperatorChoose) {
-            case "+":
-                sum = number1 + number2;
-                System.out.printf("%d + %d = %d", number1, number2, sum);
-            default:
-                System.out.printf("Invalid operator");
+        System.out.println("Enter the second number:");
+        int number2 = Integer.parseInt(keyboard.nextLine());
 
-        }
+
+        int result = calculator.UserOperator(number1, operator,number2);
+
+        //int result = UserOperator(number1, number2, operator);
+
+        System.out.printf("%d %s %d = %d\n", number1, operator, number2, result);
+
+
 
 
 //        SimpleCalculator calculator = new SimpleCalculator();
@@ -62,6 +66,22 @@ public class Start {
 
     }
 
+//    public static int UserOperator(int num1, int num2, String operator) {
+//        switch (operator) {
+//            case "+":
+//                return num1 + num2;
+//            case "-":
+//                return num1 - num2;
+//            case "*":
+//                return num1 * num2;
+//            case "/":
+//            default:
+//                System.out.println("Invalid operator.");
+//        }
+//
+//        return 0;
+//
+//    }
 }
 
 
