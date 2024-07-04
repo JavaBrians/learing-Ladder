7091e2091da6a3384f5c020470998433f5d6697c
hansmbua <hansmbua@yahoo.com>
2024-07-04 23:14:41 +0200

    learnt arrays

--- SimpleCalculator/src/ArrayTest.java
+++ SimpleCalculator/src/ArrayTest.java
@@ -8,11 +8,21 @@ public class ArrayTest {
         int[] listOfSecondNumbers = {1, 22, 84, 85, 63, 5};// 6
 //         listOfNumbers[0] = 2;
 
-        for (int i = 0; i < listOfSecondNumbers.length; i++) { // when i = 0, i+1 = 1 ;
-            listOfNumbers[i] = listOfSecondNumbers[i];
-            System.out.print(" " + listOfNumbers[i]);
+//        for (int i = 0; i < listOfSecondNumbers.length; i++) { // when i = 0, i+1 = 1 ;
+//            listOfNumbers[i] = listOfSecondNumbers[i];
+//            System.out.print(" " + listOfNumbers[i]);
+//
+//        }
+
+        for (int i = 0; i < listOfSecondNumbers.length; i++) {
+            for (int j = 0; j < listOfSecondNumbers.length; j++) {
+                listOfNumbers[i] = listOfSecondNumbers[j];//1 , 22
+            }
 
         }
+
+
+
 
 
     }
